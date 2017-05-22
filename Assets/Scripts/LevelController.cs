b13375e413422deface8b834e72fa19de0a2ec0a
pkprojects <cvalkenet@yahoo.com>
2017-05-22 14:27:12 +0200

    tweks

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -18,7 +18,7 @@ public class LevelController : MonoBehaviour {
 		cam = GameObject.FindWithTag ("MainCamera").GetComponent<Camera> ();
 		var tempLvl = levels [0];
 		background.texture = tempLvl.levelBackground;
-		Debug.Log (tempLvl.characters.Count);
+		//Debug.Log (tempLvl.characters.Count);
 		var canvas = GameObject.FindWithTag ("mainCanvas");
 		foreach (CharacterSetup tempChar in tempLvl.characters) {
 			GameObject tempGO = new GameObject ("Lmao");
@@ -27,13 +27,17 @@ public class LevelController : MonoBehaviour {
 			tempGO.GetComponent<DialogueScript>().sceneImg = tempChar.sceneImg;
 			tempGO.GetComponent<DialogueScript>().enlargedImg = tempChar.enlargedImg;
 			tempGO.AddComponent<Image>();
-			tempGO.GetComponent<Image> ().sprite = Sprite.Create( tempChar.sceneImg, new Rect(0f, 0f, tempChar.sceneImg.width, tempChar.sceneImg.height), new Vector2(0f, 0f), 100f);
+			var xRatio = (Screen.width/100) * tempChar.xPos;
+			var wRatio = (Screen.width/100) * tempChar.sceneImg.width;
+			var yRatio = (Screen.height/100) * tempChar.yPos;
+			tempGO.gameObject.transform.localScale = new Vector3 (1f, 2f, 1f);
+			tempGO.gameObject.transform.SetParent (canvas.gameObject.transform, false);
+			tempGO.GetComponent<Image> ().sprite = Sprite.Create( tempChar.sceneImg, new Rect(0f, 0f, tempChar.sceneImg.width , tempChar.sceneImg.height), new Vector2(0f, 0f), 100f);
 
-			tempGO.gameObject.transform.parent = canvas.gameObject.transform;
 			tempGO.gameObject.tag = "Character";
-			tempGO.transform.position = new Vector2 (tempChar.xPos, tempChar.yPos);
+			tempGO.transform.position = new Vector2 (xRatio, yRatio);
 		}
-		Debug.Log ("After Loops");
+		//Debug.Log ("After Loops");
 	}
 	
 	// Update is called once per frame
